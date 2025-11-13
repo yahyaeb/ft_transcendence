6ceaa7af2c2ffc60486388db1ae7ea03c8965991
yel-bouk <yahya.elboukili1@gmail.com>
2025-11-13 17:02:56 +0100

    started practicing with fastify

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+// server.js  (ESM version)
+
+import fastifyFactory from 'fastify'
+import swagger from '@fastify/swagger'
+import swaggerUI from '@fastify/swagger-ui'
+
+const fastify = fastifyFactory({ logger: true })
+const PORT = 5000
+
+fastify.register(swagger, {
+  openapi: {
+    info: { title: 'Fastify API', version: '1.0.0' }
+  }
+})
+
+fastify.register(swaggerUI, {
+  routePrefix: '/docs'
+})
+
+// example route
+fastify.get('/items', async (req, reply) => {
+  return { test: 'Hello' }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({
+      port: PORT,
+      host: '0.0.0.0'
+    })
+  } catch (error) {
+    fastify.log.error(error)
+    process.exit(1)
+  }
+}
+
+start()
