2e2f1bf650f99c6c13bef5adc0e13417cf6be2aa
Lepokile <yahya.elboukili1@gmail.com>
2025-11-18 11:54:12 -0500

    I hid the password when calling GET on users & user

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -4,13 +4,15 @@
 import { db } from '../db/database.js'
 
 export async function getAllUsersController(req, reply){
-    const users = await db.all("SELECT * FROM users;");
-    reply.send({users})
+    // const users = await db.all("SELECT * FROM users;");
+    const users = await db.all("Select id, username FROM users;");
+    return users
 }
 
 export async function getSingleUserController(req, reply){
     const {id } = req.params
-    const user = await db.get("Select * FROM users WHERE id = ?", [id]);
+    // const user = await db.get("Select * FROM users WHERE id = ?", [id]);
+    const user = await db.get("Select id, username FROM users WHERE id = ?", [id]);
 
     if(!user){
         return reply.code(404).send({error: 'User not found'})
