5a747af88fff61829930aa766b598f41cfa49a30
Nisar HAMILA <nizar.hamila06@gmail.com>
2025-11-20 16:38:55 +0100

    new front with tailwind Css

---
+++ frontend/ft_front/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
